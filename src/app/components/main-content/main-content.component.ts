44fc9ed9a5aed00b7184ab2c2babf2cbad7a6b5d
vhraposo <victorhcraposo@gmail.com>
2025-05-09 01:15:51 -0300

    Aula 12: Criando o layout das colunas de tarefas

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TaskListSectionComponent } from '../task-list-section/task-list-section.component';
 import { WelcomeSectionComponent } from '../welcome-section/welcome-section.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [WelcomeSectionComponent],
+  imports: [WelcomeSectionComponent, TaskListSectionComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.css',
 })
