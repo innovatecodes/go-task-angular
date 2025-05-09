c1f66a4e22b523daabd38126a7770fe398930b38
vhraposo <victorhcraposo@gmail.com>
2025-05-09 02:42:13 -0300

    Aula 16: Criando o layout do modal de comentários da tarefa parte 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
+import { TaskCommentsModalComponent } from './components/task-comments-modal/task-comments-modal.component';
 import { TaskFormModalComponent } from './components/task-form-modal/task-form-modal.component';
 
 @Component({
@@ -11,6 +12,7 @@ import { TaskFormModalComponent } from './components/task-form-modal/task-form-m
     HeaderComponent,
     MainContentComponent,
     TaskFormModalComponent,
+    TaskCommentsModalComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
