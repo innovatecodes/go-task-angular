67f59f74993595b9eacef81ea486e1bbb0761741
vhraposo <victorhcraposo@gmail.com>
2025-06-03 15:12:49 -0300

    Aula 30 - Configuração do Drag and Drop nas colunas de status da tarefa

--- src/app/components/task-list-section/task-list-section.component.ts
+++ src/app/components/task-list-section/task-list-section.component.ts
@@ -1,23 +1,54 @@
+import {
+  CdkDrag,
+  CdkDragDrop,
+  CdkDropList,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Component, inject } from '@angular/core';
+import { ITask } from '../../interfaces/task.interface';
 import { TaskService } from '../../services/task.service';
 import { TaskCardComponent } from '../task-card/task-card.component';
 
 @Component({
   selector: 'app-task-list-section',
-  imports: [TaskCardComponent],
+  imports: [TaskCardComponent, CdkDropList, CdkDrag],
   templateUrl: './task-list-section.component.html',
   styleUrl: './task-list-section.component.css',
 })
 export class TaskListSectionComponent {
+  todoTasks: ITask[] = [];
+  doingTasks: ITask[] = [];
+  doneTasks: ITask[] = [];
+
   private readonly _taskService = inject(TaskService);
 
   ngOnInit() {
     this._taskService.todoTasks.subscribe((todoList) => {
-      console.log('Lista de TODOS: ', todoList);
-
-      todoList[0].name = 'Nome alterado';
-
-      this._taskService.carregarListaAtualDeTodos();
+      this.todoTasks = todoList;
     });
+    this._taskService.doingTasks.subscribe((doingTasks) => {
+      this.doingTasks = doingTasks;
+    });
+    this._taskService.doneTasks.subscribe((doneTasks) => {
+      this.doneTasks = doneTasks;
+    });
+  }
+
+  drop(event: CdkDragDrop<ITask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 }
