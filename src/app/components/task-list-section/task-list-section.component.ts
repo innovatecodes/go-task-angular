7541273334fb6f74f7935b84ac3d52feb1f806ce
vhraposo <victorhcraposo@gmail.com>
2025-06-03 16:58:58 -0300

    Aula 32 - Atualizando o status da tarefa movimentada no componente

--- src/app/components/task-list-section/task-list-section.component.ts
+++ src/app/components/task-list-section/task-list-section.component.ts
@@ -7,8 +7,10 @@ import {
 } from '@angular/cdk/drag-drop';
 import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { TaskStatusEnum } from '../../enums/task-status.enum';
 import { ITask } from '../../interfaces/task.interface';
 import { TaskService } from '../../services/task.service';
+import { TaskStatus } from '../../types/task-status';
 import { TaskCardComponent } from '../task-card/task-card.component';
 
 @Component({
@@ -20,7 +22,38 @@ import { TaskCardComponent } from '../task-card/task-card.component';
 export class TaskListSectionComponent {
   readonly _taskService = inject(TaskService);
 
-  drop(event: CdkDragDrop<ITask[]>) {
+  onCardDrop(event: CdkDragDrop<ITask[]>) {
+    this.moveCardToColumn(event);
+
+    const taskId = event.item.data.id;
+    const taskCurrentStatus = event.item.data.status;
+    const droppedColumn = event.container.id;
+
+    this.updateTaskStatus(taskId, taskCurrentStatus, droppedColumn);
+  }
+
+  private updateTaskStatus(
+    taskId: string,
+    taskCurrentStatus: TaskStatus,
+    droppedColumn: string,
+  ) {
+    let taskNextStatus: TaskStatus;
+    switch (droppedColumn) {
+      case 'to-do-column':
+        taskNextStatus = TaskStatusEnum.TODO;
+        break;
+      case 'doing-column':
+        taskNextStatus = TaskStatusEnum.DOING;
+        break;
+      case 'done-column':
+        taskNextStatus = TaskStatusEnum.DONE;
+        break;
+      default:
+        throw new Error('Coluna não identificada');
+    }
+  }
+
+  private moveCardToColumn(event: CdkDragDrop<ITask[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(
         event.container.data,
