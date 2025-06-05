a49f1e568a42ef3560663a5e6e1b4c4aa6bdcc6d
vhraposo <victorhcraposo@gmail.com>
2025-06-05 10:01:15 -0300

    Aula 49 - Revisando os principais conceitos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
-import { TaskCommentsModalComponent } from './components/task-comments-modal/task-comments-modal.component';
-import { TaskFormModalComponent } from './components/task-form-modal/task-form-modal.component';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    MainContentComponent,
-    TaskFormModalComponent,
-    TaskCommentsModalComponent,
-  ],
+  imports: [HeaderComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
